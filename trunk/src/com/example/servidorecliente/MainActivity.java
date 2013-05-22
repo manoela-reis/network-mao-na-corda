6d31cd8b3002109916572dff094dee8b9f624062
manoelabarreto.nave <manoelabarreto.nave@ef5b116c-862d-18e3-cf98-a078ed533198>
2013-05-22 23:39:06 +0000

    Projeto atualizado com a classe game implementada, porém ainda contém erros

--- trunk/src/com/example/servidorecliente/MainActivity.java
+++ trunk/src/com/example/servidorecliente/MainActivity.java
@@ -31,8 +31,7 @@ public class MainActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 	}
 	
 	// Servidor/Cliente
