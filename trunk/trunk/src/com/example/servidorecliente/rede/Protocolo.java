20e28b9c865993efaf6194670903ca8c95d59f12
manoelabarreto.nave <manoelabarreto.nave@ef5b116c-862d-18e3-cf98-a078ed533198>
2013-08-17 19:07:31 +0000

    deletar para comitar

--- trunk/trunk/src/com/example/servidorecliente/rede/Protocolo.java
+++
@@ -1,8 +0,0 @@
-package com.example.servidorecliente.rede;
-
-public interface Protocolo {
-
-	public static final String PROTOCOL_ID = "ID";
-	public static final String PROTOCOL_MOVE = "MOVE";
-	public static final String PROTOCOL_CONNECT = "CONNECTED";
-}
