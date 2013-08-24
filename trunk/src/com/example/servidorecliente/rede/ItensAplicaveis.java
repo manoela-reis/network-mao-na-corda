186e65626f17ad7cbc6a5e7f1c9df314ebe26d8e
manoelabarreto.nave <manoelabarreto.nave@ef5b116c-862d-18e3-cf98-a078ed533198>
2013-08-24 17:40:45 +0000

    Interface adicionada para melhora e individualização dos métodos dos itens. mudanças e otimização no código. // corda para os dois lados

---
+++ trunk/src/com/example/servidorecliente/rede/ItensAplicaveis.java
@@ -0,0 +1,11 @@
+package com.example.servidorecliente.rede;
+
+public interface ItensAplicaveis {
+
+	public void aplicarForca(int i);
+	public void calcularItemEsp();
+	public void BotItemEsp();
+	public void createBarrinhas(int larguraBarra, int alturaBarra);
+	public void calcularItens();
+	public void BotItem();
+}
--- trunk/src/com/example/servidorecliente/rede/ItensManager.java
+++ trunk/src/com/example/servidorecliente/rede/ItensManager.java
@@ -2,7 +2,6 @@ package com.example.servidorecliente.rede;
 
 import java.util.ArrayList;
 
-import android.graphics.PointF;
 import android.graphics.Rect;
 
 public class ItensManager {
@@ -18,25 +17,37 @@ public class ItensManager {
 	// vitoria
 	private Rect ItemEsp = new Rect();
 
-	public void CreateItens(int larguraItem, int alturaItem) {
+	public void CreateItens(int larguraItem, int alturaItem, Boolean player1) {
 
 		rectsItens.add(Impulso);
 		rectsItens.add(Massa);
 		rectsItens.add(Velocidade);
 		rectsItens.add(ItemEsp);
 
-		Impulso.set(larguraItem, 6 * alturaItem, (int) (2.1f * larguraItem),
-				(int) (7.5f * alturaItem));
-		Velocidade.set((int) (2.3f * larguraItem), 6 * alturaItem,
-				(int) (3.3f * larguraItem), (int) (7.5f * alturaItem));
-		Massa.set((int) (3.5f * larguraItem), 6 * alturaItem,
-				(int) (4.5f * larguraItem), (int) (7.5f * alturaItem));
-		ItemEsp.set((int) (6.5f * larguraItem), 2 * alturaItem,
-				(int) (7.8f * larguraItem), (int) (3 * alturaItem));
+		if(player1){
+			Impulso.set(larguraItem, 6 * alturaItem, (int) (2.1f * larguraItem),
+					(int) (7.5f * alturaItem));
+			Velocidade.set((int) (2.3f * larguraItem), 6 * alturaItem,
+					(int) (3.3f * larguraItem), (int) (7.5f * alturaItem));
+			Massa.set((int) (3.5f * larguraItem), 6 * alturaItem,
+					(int) (4.5f * larguraItem), (int) (7.5f * alturaItem));
+			ItemEsp.set((int) (6.5f * larguraItem), 2 * alturaItem,
+					(int) (7.8f * larguraItem), (int) (3 * alturaItem));
+		}else{
+			Impulso.set((int)(5.5*larguraItem), 6 * alturaItem, (int) (6.5f * larguraItem),
+					(int) (7.5 * alturaItem));
+			Velocidade.set((int) (6.8 * larguraItem), 6 * alturaItem,
+					(int) (7.8 * larguraItem), (int) (7.5f * alturaItem));
+			Massa.set((int) (8 * larguraItem), 6 * alturaItem,
+					(int) (9 * larguraItem), (int) (7.5f * alturaItem));
+			ItemEsp.set((int) (6.5f * larguraItem), 2 * alturaItem,
+					(int) (7.8f * larguraItem), (int) (3 * alturaItem));
+		}
+	
 	}
 
 	public void detectar(int xPonto, int yPonto) {
-		detectado=false;
+		detectado = false;
 
 		if (rectsItens.get(0).contains(xPonto, yPonto)) {
 			// BotImpulso();
@@ -74,6 +85,40 @@ public class ItensManager {
 
 	public String ItemAtual() {
 		return StatusRect;
-	}
+	}	
+	
+	public void updateItensManager(String SegTouch, String PriTouch, Rect[]Barrinhas, DadosDoCliente dadosDoCliente, int numberSet) {
 
+		if (SegTouch == "Impulso" || PriTouch == "Impulso") {
+			if (Barrinhas[0].right - Barrinhas[0].left < 50) {
+				if (Barrinhas[0].right + 3 - Barrinhas[0].left > 50) {
+					Barrinhas[0].right = 50 + Barrinhas[0].left;
+
+				} else {
+					dadosDoCliente.setX(numberSet);
+				}
+			}
+		}
+		if (SegTouch == "Velocidade" || PriTouch == "Velocidade") {
+			if (Barrinhas[1].right - Barrinhas[1].left < 50) {
+				if (Barrinhas[1].right + 3
+						- Barrinhas[1].left > 50) {
+					Barrinhas[1].right = 50 + Barrinhas[1].left;
+
+				} else {
+					dadosDoCliente.setX(numberSet);
+				}
+			}
+		}
+		if (SegTouch == "Massa" || PriTouch == "Massa") {
+			if (Barrinhas[2].right - Barrinhas[2].left < 50) {
+				if (Barrinhas[2].right + 3 - Barrinhas[2].left > 50) {
+					Barrinhas[2].right = 50 + Barrinhas[2].left;
+
+				} else {
+					dadosDoCliente.setX(numberSet);
+				}
+			}
+		}
+	}
 }
